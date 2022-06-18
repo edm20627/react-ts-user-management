15b52e0e3c8e210f04a2785ccc3ee07165e4ac5b
edm20627 <eng00627@gmail.com>
2022-06-18 15:40:05 +0900

    メッセージ機能

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,27 +1,33 @@
-/* eslint no-alert: 0 */
 import axios from 'axios'
 import { useCallback, useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { User } from '../components/types/api/user'
+import { useMessage } from './useMessage'
 
 export const useAuth = () => {
   const history = useHistory()
 
-  const [loading, setLoading] = useState(false)
-  const login = useCallback((id: string) => {
-    setLoading(true)
+  const { showMessage } = useMessage()
 
-    axios
-      .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => {
-        if (res.data) {
-          history.push('/home')
-        } else {
-          alert('ユーザーが見つかりません。')
-        }
-      })
-      .catch(() => alert('loginできません。'))
-      .finally(() => setLoading(false))
-  }, [])
+  const [loading, setLoading] = useState(false)
+  const login = useCallback(
+    (id: string) => {
+      setLoading(true)
+
+      axios
+        .get<User>(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => {
+          if (res.data) {
+            showMessage({ title: 'ログインしました', status: 'success' })
+            history.push('/home')
+          } else {
+            showMessage({ title: 'ユーザーが見つかりません', status: 'error' })
+          }
+        })
+        .catch(() => showMessage({ title: 'loginできません', status: 'error' }))
+        .finally(() => setLoading(false))
+    },
+    [history, showMessage]
+  )
   return { login, loading }
 }
